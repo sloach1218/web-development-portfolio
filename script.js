fbe3ce8840fd021fe2ead95c3f4015516fd871fb
Sarah Loach <Sarah@Sarahs-fat-top.attlocal.net>
2019-09-29 17:04:19 -0500

    updated to accessible colors, made responsive

---
+++ script.js
@@ -0,0 +1,35 @@
+function mobileNavToggle(){
+    currentClass = document.getElementById("mobileNav");
+    $('#topNav').on('click', function () {
+        if (currentClass.className === "topnav"){
+            currentClass.className += " responsive";
+        } else {
+            currentClass.className = "topnav";
+        }
+    })
+    
+};
+$(mobileNavToggle());
+
+function mobileNavToggle2(){
+    currentClass = document.getElementById("mobileNav");
+    $('#mobileNav').on('click', function () {
+        if (currentClass.className === "topnav"){
+            currentClass.className += " responsive";
+        } else {
+            currentClass.className = "topnav";
+        }
+    })
+    
+};
+$(mobileNavToggle2());
+
+/*function altNavToggle(){
+    $('#mobileNav').hide();
+    $('#topNav').on('click', function () {
+        $('#mobileNav').show();
+
+    }
+
+}
+$(altNavToggle());*/
